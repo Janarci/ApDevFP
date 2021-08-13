4fd47d35a9113c8b67ebe549509991288c4e295d
IncogLad <vincenthe5th@yahoo.com>
2021-08-13 16:19:39 +0800

    bruh

---
+++ unity/Assets/Scripts/BGMhandler.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BGMhandler : MonoBehaviour
+{
+
+    public static BGMhandler BGM_Instance;
+
+    public Sounds[] BGMs;
+    private Sounds currentBGM;
+
+    public void Awake()
+    {
+
+        if (BGM_Instance != null && BGM_Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        BGM_Instance = this;
+        DontDestroyOnLoad(this);
+
+        foreach (Sounds s in BGMs)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+
+    }
+
+    public void setCurrentSound(string soundName)
+    {
+        currentBGM = Array.Find(BGMs, sound => sound.soundName == soundName);
+
+    }
+
+    public void play()
+    {
+        currentBGM.source.Play();
+
+    }
+
+}
+
